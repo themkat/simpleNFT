bad2e33a754b9ea91edf05504ca000971f50aa2a
Marie Katrine Ekeberg <mke@themkat.net>
2021-11-03 19:48:43 +0100

    WIP. More on the deploy scripts.

---
+++ deployContract.kts
@@ -0,0 +1,29 @@
+#!/usr/bin/env kscript
+
+import org.web3j.protocol.Web3j
+import org.web3j.protocol.http.HttpService
+import org.web3j.crypto.Credentials
+import org.web3j.tx.gas.DefaultGasProvider
+
+//DEPS org.web3j:core:4.8.7
+//INCLUDE CrypticLlamaContract.java
+
+// web3j stub CrypticLlamaContract.java must have been generated for this class to run
+
+if(1 > args.size) {
+	println("Usage:")
+	println("./deployContact.kts PRIVATE_KEY [BLOCKCHAIN_URL]")
+	println("(BLOCKCHAIN_URL is optional)")
+	System.exit(1)
+}
+
+val privateKey = args[0]
+val blockchainUrl = args.getOrNull(1)
+
+val web3j = Web3j.build(if(null == blockchainUrl)
+						HttpService()
+						else
+						HttpService(blockchainUrl))
+val credentials = Credentials.create(privateKey)
+CrypticLlamaContract.deploy(web3j, credentials, DefaultGasProvider()).send()
+web3j.shutdown()
--- deployTestNet.sh
+++ deployTestNet.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
+# TODO: print usage
+
+if [[ -z $WALLET_PRIVATE_KEY ]] then
+   echo "WALLET_PRIVATE_KEY needs to be set to be able to connect to wallet!"
+   exit 1
+fi
+   
+
 # TODO: more here
 
+
+# TODO: generate llamas
+
+
+# TODO: upload images to IPFS, then use those hashes in the metadata, then upload metadata, save hashes, inject those in smart contract
+
+# String-replace the contract so we have the correct IPFS paths
+
 # download openzeppelin contracts directly so we don't have to use npm
+rm -rf @openzeppelin
 wget -qO- https://github.com/OpenZeppelin/openzeppelin-contracts/archive/refs/tags/v4.3.2.tar.gz | tar zxvf - 
 mv openzeppelin-contracts-4.3.2 @openzeppelin
 
-# TODO: compile 
+# TODO: compile contract if  
+solc CrypticLlamaContract.sol --bin --abi --optimize -o clm
+
+# Generate Web3j stub
+web3j generate solidity -a=clm/CrypticLlamaContract.abi -b=clm/CrypticLlamaContract.bin -o=. -p= 
+
+# Deploy contract
+./deployContract.kts $WALLET_PRIVATE_KEY $BLOCKCHAIN_URL
